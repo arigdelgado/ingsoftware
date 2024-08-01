a30d147fd9df6d0f2017d0f54da001df95605eef
AndyWilliams1408 <134110811+AndyWilliams1408@users.noreply.github.com>
2024-07-31 23:21:49 -0500

    Actualización_Paciente_final

--- Citas_Medicas/App_Citas_Medicas/modelos/paciente.py
+++ Citas_Medicas/App_Citas_Medicas/modelos/paciente.py
@@ -1,11 +1,23 @@
 from django.db import models
 from .usuario import Usuario
 
+# Definición de la clase Paciente que hereda de models.Model
 class Paciente(models.Model):
+    # Campo que crea una relación uno a uno con el modelo Usuario
+    # on_delete=models.CASCADE indica que si se elimina un Usuario, se elimina el Paciente asociado
+    # related_name='paciente' permite acceder al paciente desde el modelo Usuario con usuario.paciente
     usuario = models.OneToOneField(Usuario, on_delete=models.CASCADE, related_name='paciente')
+    
+    # Campo de texto que almacena la dirección del paciente con un máximo de 255 caracteres
     direccion = models.CharField(max_length=255)
+    
+    # Campo de texto que almacena el número de teléfono del paciente con un máximo de 20 caracteres
     telefono = models.CharField(max_length=20)
+    
+    # Campo de texto que almacena el historial médico del paciente
     historial_medico = models.TextField()
 
+    # Método que define la representación en cadena del objeto Paciente
     def __str__(self):
+        # Retorna el nombre completo del usuario asociado al paciente
         return self.usuario.nombre_completo
