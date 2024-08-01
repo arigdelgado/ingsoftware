b462fc7f8c0362d6f019acc93edea8fdeae726fb
ICG17 <delgadoarianaa15@gmail.com>
2024-07-31 20:46:03 -0500

    configuracion django final

---
+++ Citas_Medicas/Citas_Medicas/urls.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Sistema de Citas Medicas",
+        default_version='v1',
+        description="Documentación con Swagger",
+        terms_of_service="https://www.google.com/policies/terms/",
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('App_Citas_Medicas.urls')),
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redocs/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc')
+]
