95d7008b793ffe26be53d65d7b02af1d8ffe45e1
arigdelgado <icordova1@est.ups.edu.ec>
2024-08-13 20:18:17 -0500

    update_test_administrador

--- Citas_Medicas/App_Citas_Medicas/servicios/serv_administrador.py
+++ Citas_Medicas/App_Citas_Medicas/servicios/serv_administrador.py
@@ -2,6 +2,47 @@ from ..repositorios.rep_administrador import AdministradorRepositorio
 from ..serializers import AdministradorSerializer
 
 class AdministradorServicio:
+    """
+    >>> from App_Citas_Medicas.modelos.usuario import Usuario
+    >>> from App_Citas_Medicas.modelos.administrador import Administrador
+    >>> from App_Citas_Medicas.servicios.serv_administrador import AdministradorServicio
+
+    >>> # Crear un usuario
+    >>> usuario = Usuario.objects.create(nombre_completo='Ximena Parra', correo='ximena_parra@gmail.com', tipo_usuario='Administrador')
+
+    >>> # Crear el servicio de administrador
+    >>> servicio = AdministradorServicio()
+
+    >>> # Crear un administrador usando el servicio
+    >>> data = {'usuario': usuario, 'permisos': 'admin'}
+    >>> administrador = servicio.crear(data)
+
+    >>> # Verificar que el administrador se creó correctamente
+    >>> administrador['usuario']['nombre_completo']
+    'Ximena Parra'
+
+    >>> # Obtener todos los administradores
+    >>> all_admins = servicio.obtener_todos()
+    >>> len(all_admins) > 0
+    True
+
+    >>> # Obtener un administrador por ID
+    >>> admin_obtenido = servicio.obtener_por_id(administrador['id'])
+    >>> admin_obtenido['usuario']['nombre_completo']
+    'Ximena Parra'
+
+    >>> # Actualizar un administrador
+    >>> data_actualizada = {'permisos': 'admin, manage, edit'}
+    >>> admin_actualizado = servicio.actualizar(administrador['id'], data_actualizada)
+    >>> admin_actualizado['permisos']
+    'admin, manage, edit'
+
+    >>> # Eliminar un administrador
+    >>> mensaje = servicio.eliminar(administrador['id'])
+    >>> mensaje['mensaje']
+    'Administrador eliminado'
+    """
+
     def __init__(self):
         self.repositorio = AdministradorRepositorio()
 
